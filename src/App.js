3a049b9d8117362c5fd6bb0aff44728b9dcad798
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-02-26 17:11:32 -0500

    Fixed syntax error in pathing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
-import Gallery from './Components/Gallery'
-import SearchBar from './Components/SearchBar'
+import Gallery from './components/Gallery'
+import SearchBar from './components/SearchBar'
 
 
 const App = () => {
